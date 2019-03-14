32851fe7edf042a1c4cb1b3e1432b33339f675d4
GYMing <407886535@qq.com>
2019-03-14 13:15:47 +0800

    [UPDATE]重写 server 设置

--- app.config.js
+++ app.config.js
@@ -1,4 +1,31 @@
 module.exports = {
+  server: {
     host: 'localhost',
-    port: 3000
+    port: 3000,
+  },
+  static: {
+    spa: true,
+    path: './dist',
+    favicon: './dist/favicon.png'
+  },
+  proxyTable: {
+    '/UCM': {
+      target: 'http://54.169.159.192',
+      secure: false,
+      changeOrigin: true
+    },
+    '/API': {
+      target: 'http://54.169.159.192:8080',
+      secure: false,
+      changeOrigin: true
+    },
+    '/test': {
+      target: 'http://localhost:8080/',
+      secure: false,
+      changeOrigin: true,
+      pathRewrite: {
+        '/test$': '/test-dev-server'
+      }
+    }
+  }
 }
--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const proxy = require('http-proxy-middleware')
+const route = require('./routes')
+const config = require('./app.config')
+const app = express()
 
-var route = require('./routes');
+const { static, proxyTable } = config
 
-var app = express();
+// Moving the http-proxy-middleware above the body-parser
+// https://github.com/chimurai/http-proxy-middleware/issues/40
+Object.keys(proxyTable).forEach(url => {
+  const option = proxyTable[url]
+  app.use(proxy(url, option))
+})
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, static.favicon)))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, static.path)))
 
-app.use('/', route);
+// use express routes
+app.use('/api', route)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+const handle404 = (function() {
+  return static.spa
+    ? (req, res) => {
+      res.sendFile(path.join(__dirname, static.path + '/index.html'))
+    }
+    : (req, res, next) => {
+      const err = new Error('Not Found')
+      err.status = 404
+      next(err)
+    }
+})()
+
+app.use(handle404)
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.send(err);
-});
+  res.status(err.status || 500)
+  res.send(err)
+})
 
-module.exports = app;
+module.exports = app
